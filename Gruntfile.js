8b436f30649b2ba0f6e68304c5d247a675fadc68
Steven Klass <sklass@pivotalenergysolutions.com>
2013-07-30 10:10:31 -0700

    Clean-up I think basebootstrap is fixed.

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function(grunt) {
         },
         jshint: {
             options: {
-                ignores: [],// HACK: workaround https://github.com/gruntjs/grunt-contrib-jshint/issues/86
+                ignores: ['js/datepicker.js'], // HACK: workaround https://github.com/gruntjs/grunt-contrib-jshint/issues/86
                 jshintrc: 'js/.jshintrc'
             },
             gruntfile: {
